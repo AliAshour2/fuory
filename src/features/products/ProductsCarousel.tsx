d6d80f4163d1c1542408a7d7edc20f91e920a4c7
ali.ashour2 <Ali.Ashour@areebgroup.com>
2025-09-10 14:09:08 +0300

    enhance product carousel

--- src/features/products/ProductsCarousel.tsx
+++ src/features/products/ProductsCarousel.tsx
@@ -4,36 +4,46 @@ import {
   CarouselItem,
   CarouselNext,
   CarouselPrevious,
-} from "@/components/ui/carousel"
-import { products } from "@/constants/products"
-import pattern from "@/assets/images/patterns/pattern.png"
+} from "@/components/ui/carousel";
+import { products } from "@/constants/products";
+import pattern from "@/assets/images/patterns/pattern.png";
 
 const ProductsCarousel = () => {
   return (
-    <section 
+    <section
       className="flex justify-center items-center  w-full h-screen"
       style={{
-        backgroundImage: `url(${pattern})`,    
+        backgroundImage: `url(${pattern})`,
+        
       }}
     >
       <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-center mb-12 text-white font-veneer">Our Fresh Products</h2>
+        <h2 className="text-7xl font-bold text-center mb-12 text-white font-veneer">
+          Our Fresh Products
+        </h2>
         <Carousel className="w-full">
           <CarouselContent>
             {products.map((product) => (
-              <CarouselItem key={product.id} className="md:basis-1/2 lg:basis-1/3">
+              <CarouselItem
+                key={product.id}
+                className="md:basis-1/2 lg:basis-1/3 animate-fade-up "
+              >
                 <div className="p-4">
-                  <div className="rounded-xl overflow-hidden shadow-lg transition-transform hover:scale-105 duration-300">
-                    <div className=" overflow-hidden">
-                      <img 
-                        src={product.image} 
+                  <div className="rounded-xl overflow-hidden  transition-transform hover:scale-105 duration-300">
+                    <div className="flex items-center justify-center overflow-hidden mx-auto">
+                      <img
+                        src={product.image}
                         alt={product.name}
                         className="w-full h-full object-cover"
                       />
                     </div>
                     <div className="py-6 pb-6 text-center">
-                      <h3 className="text-2xl font-bold mb-2">{product.name}</h3>
-                      <p className="text-gray-600">{product.description}</p>
+                      <h3 className="text-3xl font-bold mb-2 text-white font-bebas">
+                        {product.name}
+                      </h3>
+                      <p className="text-gray-200 font-bebas">
+                        {product.description}
+                      </p>
                     </div>
                   </div>
                 </div>
@@ -45,7 +55,9 @@ const ProductsCarousel = () => {
         </Carousel>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default ProductsCarousel
+export default ProductsCarousel;
+
+
