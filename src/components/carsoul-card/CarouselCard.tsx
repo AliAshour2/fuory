d1647822a290ee26719cc35e56d97ce8d41444e7
ali.ashour2 <Ali.Ashour@areebgroup.com>
2025-09-30 20:39:22 +0300

    enhance the card ui

--- src/components/carsoul-card/CarouselCard.tsx
+++ src/components/carsoul-card/CarouselCard.tsx
@@ -7,20 +7,32 @@ interface CarouselCardProps {
 const CarouselCard = ({ image, name, description }: CarouselCardProps) => {
   return (
     <div className="p-4">
-      <div className="rounded-xl overflow-hidden  transition-transform hover:scale-105 duration-300">
-        <div className="flex items-center justify-center overflow-hidden mx-auto">
+      <div className="group rounded-xl overflow-hidden relative">
+        {/* Image */}
+        <div className="flex items-center justify-center h-96 w-full">
           <img
             src={image}
             alt={name}
-            className="w-full h-full object-cover"
+            className="w-full h-full object-contain transition-transform duration-500 group-hover:scale-105"
           />
         </div>
-        <div className="py-6 pb-6 text-center">
+
+        {/* Overlay (Name + Description) */}
+        <div className="absolute -bottom-7 group-hover:-bottom-4 left-0 w-full p-4 text-center   transition-all duration-500 ease-in-out">
           <h3 className="text-3xl font-bold mb-2 text-white font-bebas">
             {name}
           </h3>
+
           {description && (
-            <p className="text-gray-200 font-bebas">{description}</p>
+            <p
+              className="
+                text-gray-200 font-bebas opacity-0 translate-y-6 
+                transition-all duration-500 ease-in-out 
+                group-hover:opacity-100 group-hover:translate-y-0
+              "
+            >
+              {description}
+            </p>
           )}
         </div>
       </div>
